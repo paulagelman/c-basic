ca4cabc416bae2bfb4d380af3a993ed4d1500892
Paula <paulagel2006@gmail.com>
2025-11-17 11:08:19 +0200

    first commit

---
+++ calc_age.c
@@ -0,0 +1,19 @@
+/*------------------------------------------------------
+* Filename: calc_age.c
+* Description: the program calculate the age of the user
+* Author: Paula Gelman
+-------------------------------------------------------*/
+#include <stdio.h>
+int main() {
+    int curr_year;
+    int curr_month;
+    int curr_day;
+
+    int birth_year;
+    int birth_month; 
+    int birth_day;
+    printf("Enter current date:");  
+     scanf("%d%d%d",&curr_year,&curr_month);
+    printf("Enter birthday:");  
+    return 0;
+}
---
+++ max.c
@@ -0,0 +1,13 @@
+/*------------------------------------------------------
+* Filename: max.c
+* Description: the program prints the bigger number
+* Author: Paula Gelman
+-------------------------------------------------------*/
+#include <stdio.h>
+int main() {
+    int x=7;
+    int y=3;
+    int z= x*(x>y) + y*(y>x);/*only one of the conditions will be 1*/
+    printf("The bigger number is:%d",z);
+    return 0;
+}
